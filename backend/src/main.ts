15a354b8a241d047cb8d15698e70c73fc02a02b5
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-05-10 20:17:57 +0330

    Feat: Cors added to main!

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({transform:true,whitelist:true}))
+  app.enableCors({origin:'*'})
   const configService=app.get(ConfigService)
   const port=configService.get<number>('PORT') ?? 5550
   await app.listen(port);

505681de167bb185e83c238acfeb9e97bdf20e6e
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-05-11 21:05:37 +0330

    Feat: product schema and dto

--- backend/src/products/dto/CreateProduct.dto.ts
+++ backend/src/products/dto/CreateProduct.dto.ts
@@ -1,53 +1,58 @@
 import {
-  ArrayNotEmpty,
-  IsArray,
-  IsMongoId,
-  IsNotEmpty,
-  IsNumber,
-  isString,
   IsString,
-  Max,
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  IsEnum,
+  IsMongoId,
+  IsArray,
   Min,
+  Max,
 } from 'class-validator';
+import { Type } from 'class-transformer';
+import { Size } from 'src/schemas/Product.schema';
+
 
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty({ message: 'Product title is required!' })
   title: string;
 
-  @IsArray()
-  @ArrayNotEmpty({ message: 'Product image is required!' })
-  @IsString({ each: true })
-  images: string[];
+  @IsString()
+  @IsNotEmpty({ message: 'Product image is required!' })
+  image: string;
 
   @IsArray()
-  @IsMongoId({
-    each: true,
-    message: 'Each category ID should be a valid mongoDB objectId!',
-  })
+  @IsMongoId({ each: true })
   categories: string[];
 
   @IsString()
-  @IsNotEmpty({ message: 'Product description is required!' })
+  @IsNotEmpty({ message: 'Description is required!' })
   description: string;
 
   @IsNumber()
-  @Min(0, { message: 'Price must be a positive number!' })
+  @Type(() => Number)
+  @IsNotEmpty({ message: 'Price is required!' })
   price: number;
 
   @IsNumber()
-  @Min(0, { message: 'Rating should be a positive number!' })
-  @Max(5)
-  rating: number = 0;
-
-  @IsNumber()
-  @Min(0, { message: 'Review should be a positive number!' })
-  review: number = 0;
+  @Type(() => Number)
+  @IsOptional()
+  discount?: number;
 
   @IsArray()
-  @IsMongoId({
-    each: true,
-    message: 'Each tag ID should be a valid mongoDB objectId!',
-  })
+  @IsMongoId({ each: true })
   tags: string[];
+
+  @IsEnum(Size, { message: 'Invalid size value' })
+  size: Size;
+
+  @IsNumber()
+  @Type(() => Number)
+  @IsOptional()
+  quantity?: number;
+
+  @IsString()
+  @IsNotEmpty({ message: 'SKU is required!' })
+  sku: string;
 }
--- backend/src/schemas/Product.schema.ts
+++ backend/src/schemas/Product.schema.ts
@@ -3,6 +3,14 @@ import mongoose, { Types } from 'mongoose';
 import { Category } from './Category.schema';
 import { Tag } from './Tag.schema';
 
+export enum Size {
+  MEDIUM = 'medium',
+  TALL = 'tall',
+  STANDARD = 'standard',
+  SEMI = 'semi',
+  MINI = 'mini',
+}
+
 @Schema({ timestamps: true })
 export class Product {
   @Prop({
@@ -11,8 +19,8 @@ export class Product {
   })
   title: string;
 
-  @Prop({ required: [true, 'Product image is required!'], default: [] })
-  images: string[];
+  @Prop({ required: [true, 'Product image is required!'] })
+  image: string;
 
   @Prop({
     type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
@@ -29,6 +37,9 @@ export class Product {
   })
   price: number;
 
+  @Prop({ default: 0 })
+  discount: number; // In percent
+
   @Prop({
     required: [true, 'Rating is required!'],
     default: 0,
@@ -43,8 +54,23 @@ export class Product {
   })
   review: number;
 
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }] })
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }],
+  })
   tags: Tag[];
+
+  @Prop({
+    required: [true, 'Size is required!'],
+    enum: Size,
+    // default: Size.MEDIUM,
+  })
+  size: Size;
+
+  @Prop({ required: [true, 'Quantity is required!'], default: 1 })
+  quantity: number;
+
+  @Prop({ required: [true, 'SKU is required!'] })
+  sku: string;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);

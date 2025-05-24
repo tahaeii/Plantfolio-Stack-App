52c04893207de1a666a7b0a50bfe7ff018109270
masoomeh ghorbani <masoomeh77ghorbani98@gmail.com>
2025-05-24 18:46:05 +0330

    Fix: User Module

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -5,13 +5,16 @@ import { User, UserSchema } from 'src/schemas/User.schema';
 import { UsersController } from './users.controller';
 import { AdminStrategy } from 'src/auth/strategy/admin.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({})
   ],
-  providers: [UsersService, AdminStrategy],
+  providers: [UsersService, AdminStrategy,JwtStrategy],
   controllers: [UsersController],
 })
 export class UsersModule {}
